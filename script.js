b612df9fc2128cdac706c502ded41eeede550681
Krosvellos <daniel.abraham@seznam.cz>
2023-01-12 14:35:52 +0100

    menší úprava

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-const menuBtn = document.querySelector('.menuHamburger')
-let menuOpen = false;
+
+
+
+   const menuBtn = document.querySelector('.menuHamburger')
+   let menuOpen = false;
+   menuBtn.addEventListener('click', () => {
+       if(!menuOpen) {
+           menuBtn.classList.add('open');
+           menuOpen = true;
+       } else {
+           menuBtn.classList.remove('open');
+           menuOpen = false;
+       }
+   });
+   const list = document.querySelector('.rightBarStuff');
 menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
+    list.classList.toggle('show');
+});
+
+
+const qq = document.querySelector(".dropdownContent");
+function s() {
+        qq.style.display = "flex";
+  }
+
+  function c() {
+        qq.style.display = "none";
+}
+
+function showOrHide() {
+    if (qq.style.display === "none") {
+      s()
     } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
+      c()
     }
-});
-const list = document.querySelector('.rightBarStuff');
-menuBtn.addEventListener('click', () => {
- list.classList.toggle('show');
-});
+  }
